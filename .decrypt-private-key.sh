2c290d08f5fb87f2c1dc6171bf79816d645f3a21
Ivan Molodetskikh <yalterz@gmail.com>
2025-05-23 19:49:27 +0200

    cleanup

--- .decrypt-private-key.sh
+++ .decrypt-private-key.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-if [ ! -f "${HOME}/.config/chezmoi/key.txt" ]; then
+#if [ ! -f "${HOME}/.config/chezmoi/key.txt" ]; then
 #    mkdir -p "${HOME}/.config/chezmoi"
 #    chezmoi age decrypt --output "${HOME}/.config/chezmoi/key.txt" --passphrase "${CHEZMOI_SOURCE_DIR}/key.txt.age"
 #    chmod 600 "${HOME}/.config/chezmoi/key.txt"
-fi
+#fi
+
+echo "hello from decrypt-private-key.sh"
